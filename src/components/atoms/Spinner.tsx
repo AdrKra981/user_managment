7b1ed1188f79abd585ec7ca4855863f5e8685ac3
AdrKra981 <krawczykadrian97@gmail.com>
2024-09-04 16:06:25 +0200

    last fix with spinner

--- src/components/atoms/Spinner.tsx
+++ src/components/atoms/Spinner.tsx
@@ -1,6 +1,8 @@
 const Spinner = () => {
   return (
-    <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-blue-600" />
+    </div>
   );
 };
 
